082b3d490fce7ac2319075a77b8cd1b92245acee
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-10-27 20:47:12 +0500

    GUI

---
+++ GUI/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Driver Code
+int main(int argc, char* argv[])
+{
+    std::cout << "cplusplus version : "<< __cplusplus << "\n";
+    std::cout << "Hello, World!" << "\n";
+    return 0;
+}
+
+
+
+
